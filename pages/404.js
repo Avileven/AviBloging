1446ea67d1a399e06304cb14758c59157505e004
Avi Leventhal <84894397+Avileven@users.noreply.github.com>
2021-10-14 08:40:12 +0300

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -7,7 +7,7 @@ const NotFound = () => {
        
         <Image src="/eror.png" alt="site logo" width={300} height={130} />
       <h1>Sorry</h1>
-      <h2>you are lost....  :)</h2>
+      <h2>you are lost</h2>
       <p>Go back to the <Link href="/"><a>Homepage</a></Link></p>
     </div>
   );
